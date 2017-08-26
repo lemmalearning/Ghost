ddb67596e2592c0b72a942a588a8f490a7b88c4d
Dennis Shtatnov <densht@gmail.com>
2017-08-26 13:49:18 -0400

    Add missing build files

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,7 @@ rm -rf core/built
 
 grunt prod
 
-tar -zcvf ghost-built.tar.gz core/built
+tar -zcvf ghost-built.tar.gz core/built core/server/admin/views core/server/public/ghost-sdk.min.js core/server/public/ghost.min.css
 gsutil cp ghost-built.tar.gz gs://lm-assets/
 
 git push google master -f
-
-#gcloud container builds submit --config cloudbuild.yaml .
